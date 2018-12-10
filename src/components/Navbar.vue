9dcb49db1238357ce5dd63955290d4fc9040dfa2
Vladimir Striber <praksa.1@itengine.eu>
2018-12-10 16:53:07 +0100

    This is the initial commit.

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+
+<div class="navbar">
+    <ul><li class="red-text">Home</li></ul>
+</div>
+    
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
